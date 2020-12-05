df7942547ef47670a174a1e6a9186f1043913774
Adrian Novegil <adrian.novegil@gmail.com>
2020-12-06 00:07:26 +0100

    Reseting the project configurations

---
+++ microservices-friend-recomendation-event-sourcing/mfres-config-service/src/test/java/com/devcircus/java/microservices/mfres/config/ConfigServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devcircus.java.microservices.mfres.config;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
